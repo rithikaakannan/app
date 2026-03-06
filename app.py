96af7abdaf8eaf81e693e77809d75faa59e6a6b2
rithikaakannan <pdyriths01@gmail.com>
2026-03-06 12:04:45 +0530

    Refactor login page and improve UI elements

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from assignments import faculty_assignments, student_assignments
 
 st.set_page_config(page_title="CLMS", layout="wide")
 
-# session state
+# SESSION STATE
 if "role" not in st.session_state:
     st.session_state["role"] = None
 
@@ -17,58 +17,15 @@ if "role" not in st.session_state:
 # ---------- LOGIN PAGE ----------
 if st.session_state["role"] is None:
 
-    # Background Style
-    page_bg = """
-    <style>
-
-    [data-testid="stAppViewContainer"]{
-    background-image: url("https://images.unsplash.com/photo-1501785888041-af3ef285b470");
-    background-size: cover;
-    background-position: center;
-    }
-
-    .login-box{
-    background: rgba(255,255,255,0.1);
-    padding:40px;
-    border-radius:20px;
-    backdrop-filter: blur(15px);
-    width:400px;
-    margin:auto;
-    margin-top:120px;
-    text-align:center;
-    box-shadow:0px 0px 30px rgba(0,0,0,0.4);
-    }
-
-    .title{
-    font-size:32px;
-    color:white;
-    font-weight:bold;
-    margin-bottom:10px;
-    }
-
-    .subtitle{
-    color:white;
-    margin-bottom:20px;
-    }
-
-    </style>
-    """
-
-    st.markdown(page_bg, unsafe_allow_html=True)
-
-    st.markdown('<div class="login-box">', unsafe_allow_html=True)
-
-    st.markdown('<div class="title">CLMS Login</div>', unsafe_allow_html=True)
-    st.markdown('<div class="subtitle">Centralized Learning Management System</div>', unsafe_allow_html=True)
+    st.title("🎓 Centralized Learning Management System")
 
     role = st.radio("Login As", ["Student", "Faculty"])
 
     if role == "Student":
         student_login()
+
     else:
         faculty_login()
-
-    st.markdown('</div>', unsafe_allow_html=True)
 
 
 # ---------- AFTER LOGIN ----------
@@ -86,7 +43,8 @@ else:
 
     if selected == "Home":
         st.title("🏫 Welcome to CLMS")
-        st.write("Welcome", st.session_state["user"])
+        st.write("Welcome:", st.session_state["user"])
+
 
     elif selected == "Dashboard":
 
@@ -95,12 +53,14 @@ else:
         else:
             show_faculty_dashboard()
 
+
     elif selected == "Attendance":
 
         if st.session_state["role"] == "student":
             student_attendance()
         else:
             faculty_attendance()
+
 
     elif selected == "Assignments":
 
@@ -109,14 +69,17 @@ else:
         else:
             faculty_assignments()
 
+
     elif selected == "Announcements":
         st.title("📢 Announcements")
         st.info("Mid semester exams start next week")
+
 
     elif selected == "Profile":
         st.title("👤 Profile")
         st.write("User:", st.session_state["user"])
         st.write("Role:", st.session_state["role"])
+
 
     elif selected == "Logout":
         st.session_state["role"] = None
