94bc7f9194c1676ffc03c51cb92245b2880fbf05
rithikaakannan <pdyriths01@gmail.com>
2026-03-06 12:23:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,78 +1,35 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-
 from login import student_login, faculty_login
-from student_dashboard import show_student_dashboard
-from faculty_dashboard import show_faculty_dashboard
-from attendance import faculty_attendance, student_attendance
-from assignments import faculty_assignments, student_assignments
+from student_dashboard import student_dashboard
+from faculty_dashboard import faculty_dashboard
 
 st.set_page_config(page_title="CLMS", layout="wide")
 
-# SESSION STATE
 if "role" not in st.session_state:
     st.session_state.role = None
 
-# ---------------- LOGIN PAGE ----------------
+st.title("🎓 Centralized Learning Management System")
 
+# LOGIN PAGE
 if st.session_state.role is None:
 
-    st.title("🎓 Centralized Learning Management System")
+    option = st.radio("Login As", ["Student", "Faculty"])
 
-    role = st.radio("Login As", ["Student", "Faculty"])
-
-    if role == "Student":
+    if option == "Student":
         student_login()
+
     else:
         faculty_login()
 
-# ---------------- DASHBOARD ----------------
-
+# DASHBOARD PAGE
 else:
 
-    with st.sidebar:
+    if st.session_state.role == "student":
+        student_dashboard()
 
-        selected = option_menu(
-            "CLMS Menu",
-            ["Home", "Dashboard", "Attendance", "Assignments", "Announcements", "Profile", "Logout"],
-            icons=["house", "speedometer", "calendar", "book", "megaphone", "person", "box-arrow-right"],
-            default_index=0
-        )
+    if st.session_state.role == "faculty":
+        faculty_dashboard()
 
-    if selected == "Home":
-        st.title("🏫 Welcome to CLMS")
-        st.write("Logged in as:", st.session_state.user)
-
-    elif selected == "Dashboard":
-
-        if st.session_state.role == "student":
-            show_student_dashboard()
-        else:
-            show_faculty_dashboard()
-
-    elif selected == "Attendance":
-
-        if st.session_state.role == "student":
-            student_attendance()
-        else:
-            faculty_attendance()
-
-    elif selected == "Assignments":
-
-        if st.session_state.role == "student":
-            student_assignments()
-        else:
-            faculty_assignments()
-
-    elif selected == "Announcements":
-        st.title("📢 Announcements")
-        st.info("Mid semester exams start next week")
-
-    elif selected == "Profile":
-        st.title("👤 Profile")
-        st.write("User:", st.session_state.user)
-        st.write("Role:", st.session_state.role)
-
-    elif selected == "Logout":
+    if st.sidebar.button("Logout"):
         st.session_state.role = None
         st.rerun()
